25f5541f79587e9d39dd5792d5b7e13675ed3193
prof64 <80442784+prof64@users.noreply.github.com>
2022-09-15 22:28:36 -0400

    Minify outputed dexes
    The compile_build.py script now outputs dexes sans any whitespace. This should decrease file sizes.

--- compile_build.py
+++ compile_build.py
@@ -1,48 +1,11 @@
 # A script that compiles collections of subdexes as specified by the config.json
+import json
 import commentjson
 import sys
 import os, os.path
 from pathlib import Path
 import copy
 from jsonpath_ng import parse
-
-# custom to_json method
-def to_json(o, level=0):
-  INDENT = 2
-  SPACE = " "
-  NEWLINE = "\n"
-  ret = ""
-  if isinstance(o, dict):
-    ret += "{" + NEWLINE
-    comma = ""
-    for k, v in o.items():
-      ret += comma
-      comma = ",\n"
-      ret += SPACE * INDENT * (level + 1)
-      ret += '"' + str(k) + '":' + SPACE
-      ret += to_json(v, level + 1)
-    ret += NEWLINE + SPACE * INDENT * level + "}"
-  elif isinstance(o, str):
-    if(o == "\u0000"): # properly escape null terminator
-      ret += '"\\u0000"'
-    else:
-      ret += '"' + o + '"'
-  elif isinstance(o, list):
-    ret += "[" + ", ".join([to_json(e, level + 1) for e in o]) + "]"
-  # Tuples are interpreted as lists
-  elif isinstance(o, tuple):
-    ret += "[" + ", ".join(to_json(e, level + 1) for e in o) + "]"
-  elif isinstance(o, bool):
-    ret += "true" if o else "false"
-  elif isinstance(o, int):
-    ret += str(o)
-  elif isinstance(o, float):
-    ret += '%.7g' % o
-  elif o is None:
-    ret += 'null'
-  else:
-    raise TypeError("Unknown type '%s' for json serialization" % str(type(o)))
-  return ret
 
 # Merges two dictionaries in-place.
 # Note: If the a key corresponds to an array in both dicts, the elements of
@@ -160,7 +123,7 @@ for dexType, dexList in dexes.items():
 
   # post-processing, write file
   with open(os.path.join(build_path,'{0}Dex.json'.format(dexType)), 'w', encoding='utf-8') as f:
-    f.write(to_json(base))
+    f.write(json.dumps(base,separators=(',', ':')))
   print("Compiled {0}Dex...".format(dexType))
 
 
@@ -177,6 +140,6 @@ for dexType in dexes:
 
 # add new config to build
 with open(os.path.join(build_path,'config.json'), 'w', encoding='utf-8') as f:
-  f.write(to_json(newconfig))
+  f.write(json.dumps(newconfig,separators=(',', ':')))
 
 print("\nFinished compiling dexes!")
